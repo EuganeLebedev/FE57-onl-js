3b513c347a056e50c158dde2b68fb61415a1b861
Eugane Lebedev <e.lebedev@icode.by>
2024-02-09 10:42:33 +0300

    feat(hw_1): complete task 3

---
+++ hw_2/task_3.js
@@ -0,0 +1,13 @@
+let count = 42
+let userName = '42'
+
+countString1 = count.toString()
+countString2 = count.toString()
+
+console.log(typeof countString1 === 'string', typeof countString2 === 'string')
+
+userNameNum1 = Number(userName)
+userNameNum2 = parseInt(userName) 
+
+console.log(typeof userNameNum1 === "number", typeof userNameNum2 === 'number')
+
