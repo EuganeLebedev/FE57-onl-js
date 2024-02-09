7ba2330b16c12102c75a3f4f1ea7872a6bda2bcb
Eugane Lebedev <e.lebedev@icode.by>
2024-02-09 10:28:24 +0300

    feat(hw_1): complete task 2

---
+++ hw_2/task_2.js
@@ -0,0 +1,11 @@
+
+let secInMinutes = 60
+let minutesInHours = 60
+let hoursInDay = 24
+let daysInYear = 365
+
+let myAge = prompt("Age?:")
+
+let myAgeInSeconds = myAge * daysInYear * hoursInDay * minutesInHours * secInMinutes
+
+console.log(myAgeInSeconds)
