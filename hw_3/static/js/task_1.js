0bd9c61bb1b518c91fec84b4c39d2e274f23036b
Eugane Lebedev <e.lebedev@icode.by>
2024-02-21 17:25:20 +0300

    feat(hw_3): complete task 1

---
+++ hw_3/static/js/task_1.js
@@ -0,0 +1,11 @@
+let my_variable1 = 'true'
+let my_variable2 =  false
+let my_variable3 =  17
+let my_variable4 =  undefined
+let my_variable5 =  null
+
+console.log(`Variable: my_variable1 have type: ${typeof my_variable1}`)
+console.log(`Variable: my_variable2 have type: ${typeof my_variable2}`)
+console.log(`Variable: my_variable3 have type: ${typeof my_variable3}`)
+console.log(`Variable: my_variable4 have type: ${typeof my_variable4}`)
+console.log(`Variable: my_variable5 have type: ${typeof my_variable5}`)
