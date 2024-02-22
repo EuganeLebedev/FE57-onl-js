aa2bbf6d4c1a83076214f6b7bf134e25eaff99e4
Eugane Lebedev <e.lebedev@icode.by>
2024-02-22 09:09:10 +0300

    feat(hw_3): complete task 6

---
+++ hw_3/static/js/task_6.js
@@ -0,0 +1,14 @@
+console.log("Task 6");
+
+let age = prompt("Age?");
+
+num = Number(age)
+if (age > 18) {
+    console.log("Попей пивка")
+}
+else {
+    console.log("Пей колу")
+}
+if ( 16 <= age && age <= 18) {
+    console.log("Можешь выкурить сигаретку, только маме не говори")
+}
